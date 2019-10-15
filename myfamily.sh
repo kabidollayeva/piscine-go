3e949b775d7a7baeca2e7768a78b77f54efefd56
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-15 21:52:12 +0600

    c1-camp4

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[] | select (.id == 1) | .connections | .relatives' | sed 's/"/ /g'
+curl https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq '.[] | select (.id == 1) | .connections | .relatives' | sed 's/"/ /g' | sed 's/ //'
