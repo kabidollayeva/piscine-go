b3792fbd1bd0e8fe2fb3b197a70e14c0dbe06194
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-16 20:50:36 +0600

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,13 +3,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := 0; i <= 7; i++ {
-		for j := i + 1; j <= 8; j++ {
-			for k := j + 1; k <= 9; k++ {
+	for i := 48; i <= 55; i++ {
+		for j := i + 1; j <= 56; j++ {
+			for k := j + 1; k <= 57; k++ {
 				z01.PrintRune(rune(i))
 				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))
-				if i < 7 {
+				if i < 55 {
 					z01.PrintRune(',')
 				} else {
 					z01.PrintRune('\n')
@@ -18,6 +18,7 @@ func PrintComb() {
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
 
 func main() {
