01f50a821d245a1d3b88a096e02872534a5912d4
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-17 21:04:18 +0600

    printstr

---
+++ printstr.go
@@ -0,0 +1,12 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintStr(str string) {
+
+	aString := []rune(str)
+	for _, str := range aString {
+		z01.PrintRune(str)
+	}
+	z01.PrintRune('\n')
+}
