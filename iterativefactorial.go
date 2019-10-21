54b2e2c2eed481a6de8d7b5d91593215a4c4d3d8
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-21 16:36:18 +0600

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,13 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	for i := 1; i <= nb; i++ {
-		result = result * i
+	if nb == 0 || nb == 1 {
+		result = 0
+		return result
+	} else {
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+		return result
 	}
-	return result
 }
