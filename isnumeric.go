b48d20dcd21f3a9aa08a0c30472461daad2ecbf4
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-22 23:24:49 +0600

    isnumeric

---
+++ isnumeric.go
@@ -0,0 +1,15 @@
+package piscine
+
+func IsNumeric(str string) bool {
+	runes := []rune(str)
+	counter := 0
+	for _, letter := range runes {
+		if Check(letter) {
+			counter++
+		}
+	}
+	if counter == CheckStr(str) {
+		return true
+	}
+	return false
+}
