23b101b82f0e03225ff8f009edd960a5e831d3f7
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-22 21:01:45 +0600

    nrune

--- nrune.go
+++ nrune.go
@@ -1,8 +1,18 @@
 package piscine
 
+func Length(s string) int {
+	counter := 0
+	for i := range s {
+		if i == i {
+			counter++
+		}
+	}
+	return counter
+}
 func NRune(s string, n int) rune {
 	sentence := []rune(s)
-	if n <= 9 {
+	len := Length(s)
+	if n <= len && n-1 >= 0 {
 		return sentence[n-1]
 	} else {
 		return 0
