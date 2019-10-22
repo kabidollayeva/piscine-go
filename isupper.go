e4fa52ac5d4542500ebb7bd3afa31405d02ba52a
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-22 23:32:44 +0600

    isupper

---
+++ isupper.go
@@ -0,0 +1,22 @@
+package piscine
+
+func CheckU(r rune) bool {
+	if r >= 'A' && r <= 'Z' {
+		return true
+	}
+	return false
+}
+
+func IsLower(str string) bool {
+	runes := []rune(str)
+	counter := 0
+	for _, letter := range runes {
+		if CheckU(letter) {
+			counter++
+		}
+	}
+	if counter == CheckStr(str) {
+		return true
+	}
+	return false
+}
