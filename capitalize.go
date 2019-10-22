26cccf9acc4a04547cf3becfddd7aae8e751ffbc
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-22 22:58:18 +0600

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,15 +1,25 @@
 package piscine
 
+func Check(r rune) bool {
+	if r >= 'A' && r <= 'Z' || r >= 'a' && r <= 'z' || r >= '0' && r <= '9' {
+		return true
+
+	}
+	return false
+}
 func Capitalize(s string) string {
 	runes := []rune(s)
-	if runes[0] >= 'a' && runes[0] <= 'z' {
-		runes[0] = runes[0] - 32
-	}
-	for i := 1; i < Length(runes); i++ {
-		if runes[i] >= 'a' && runes[i] <= 'z' && ((runes[i-1] > '9' || runes[i-1] < '0') && runes[i] > 'z' || runes[i] < 'a' || runes[i] > 'Z' || runes[i] < 'A') {
-			runes[i] -= 32
-		} else if runes[i] >= 'A' && runes[i] <= 'Z' && ((runes[i-1] > '9' || runes[i-1] < '0') && runes[i] > 'Z' || runes[i] < 'A' || runes[i] > 'z' || runes[i] < 'a') {
-			runes[i] += 32
+	for index, letter := range runes {
+		if Check(letter) {
+			if index == 0 || Check(runes[index-1]) == false {
+				if letter >= 'a' && letter <= 'z' {
+					runes[index] -= 32
+				} else {
+					if letter >= 'A' && letter <= 'Z' {
+						runes[index] += 32
+					}
+				}
+			}
 		}
 
 	}
