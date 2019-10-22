e187549d21a5082c4578fa328bbb7c3cf6176aaa
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-22 23:27:54 +0600

    isnumeric

--- isnumeric.go
+++ isnumeric.go
@@ -1,10 +1,17 @@
 package piscine
 
+func CheckN(r rune) bool {
+	if r >= '0' && r <= '9' {
+		return true
+	}
+	return false
+}
+
 func IsNumeric(str string) bool {
 	runes := []rune(str)
 	counter := 0
 	for _, letter := range runes {
-		if Check(letter) {
+		if CheckN(letter) {
 			counter++
 		}
 	}
