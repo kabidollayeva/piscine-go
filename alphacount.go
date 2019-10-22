d5e8a51f9ce3d09f261218bfac9ebda6bbcee735
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-22 15:46:47 +0600

    alphacount

---
+++ alphacount.go
@@ -0,0 +1,12 @@
+package main
+
+func AlphaCount(str string) int {
+	counter := 0
+	for _, letter := range str {
+		if (letter >= 'a' && letter <= 'z') || (letter >= 'A') && (letter <= 'Z') {
+			counter++
+		}
+	}
+	return counter
+
+}
