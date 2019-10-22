1e4855e5a8c643e9a64387e6de54d41539349f0a
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-22 16:41:04 +0600

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,18 +3,22 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
-	var a []int
-	if n == 0 {
-		a = append(a, 0)
-	}
-	for i := 0; n > 0; i++ {
-		a = append(a, n%10)
-		n /= 10
-	}
-	for index, num := range a {
-		if num != 0 {
-			for i := 0; i <= num; i++ {
-				z01.PrintRune(rune(index + a[i]))
+	var a [10]int
+	if n < 10 && n >= 0 {
+		z01.PrintRune(rune(n + 48))
+	} else {
+		for {
+			if n == 0 {
+				break
+			}
+			a[n%10]++
+			n /= 10
+		}
+		for index, num := range a {
+			if num != 0 {
+				for i := 0; i < num; i++ {
+					z01.PrintRune(rune(index + 48))
+				}
 			}
 		}
 	}
