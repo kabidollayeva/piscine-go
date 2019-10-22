aa57b618320454391bd445498651fa93bad3360a
kabidollayeva <zhuldyz.kabidollayeva@nu.edu.kz>
2019-10-22 23:29:59 +0600

    islower

--- islower.go
+++ islower.go
@@ -1,10 +1,17 @@
 package piscine
 
+func CheckL(r rune) bool {
+	if r >= 'a' && r <= 'z' {
+		return true
+	}
+	return false
+}
+
 func IsLower(str string) bool {
 	runes := []rune(str)
 	counter := 0
 	for _, letter := range runes {
-		if Check(letter) {
+		if CheckL(letter) {
 			counter++
 		}
 	}
